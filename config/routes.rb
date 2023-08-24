8aafd4bebb2cae001d4b7d5c81c6882b43d0ea48
Neptune Noise <Megadimension@yandex.ru>
2023-08-25 01:28:33 +0300

    Add "About me" page. Fix and Add logo images

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'sorts#index', as: 'home'
 
-  get 'about_me' => 'pages#about_me'
+  get 'about_me' => 'pages#about_me', as: 'about'
   resources :sorts
 end
