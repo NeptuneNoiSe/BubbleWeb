69edebbb0f402d68668f4f40e2bd3a8a2e8e2ef4
d.a.arhipov <megadimension@Yandex.ru>
2023-09-14 17:18:34 +0300

    Add message in text_area, Create new view for version history, update links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,13 @@ class PagesController < ApplicationController
   def about_me
     render layout: 'about'
   end
+  def developer
+    render layout: 'developer'
+  end
+  def random
+    render layout: 'random'
+  end
+  def support
+    render layout: 'support'
+  end
 end
