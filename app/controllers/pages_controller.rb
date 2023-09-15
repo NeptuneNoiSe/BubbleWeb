684ae426c9b4f27c02246a5754db4946d145dac9
d.a.arhipov <megadimension@Yandex.ru>
2023-09-15 12:47:00 +0300

    All links work, add element "Work in progress"

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,13 +2,40 @@ class PagesController < ApplicationController
   def about_me
     render layout: 'about'
   end
-  def developer
-    render layout: 'developer'
-  end
+
   def random
     render layout: 'random'
   end
+
   def support
     render layout: 'support'
+  end
+
+  def developer
+    render layout: 'developer'
+  end
+
+  def contacts
+    render layout: 'contacts'
+  end
+
+  def platform
+    render layout: 'platform'
+  end
+
+  def resources
+    render layout: 'resources'
+  end
+
+  def another_projects
+    render layout: 'another_projects'
+  end
+
+  def future_projects
+    render layout: 'future_projects'
+  end
+
+  def see_also
+    render layout: 'see_also'
   end
 end
