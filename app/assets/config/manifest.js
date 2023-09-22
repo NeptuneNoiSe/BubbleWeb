90703cdababbb06434606196b10cd3a8813c1ffd
d.a.arhipov <megadimension@Yandex.ru>
2023-09-22 13:13:20 +0300

    Add Nav Bar and fixed buttons

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,5 @@
 //= link_tree ../images
 //= link color-modes.js
-//= link theme_switch.js
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
