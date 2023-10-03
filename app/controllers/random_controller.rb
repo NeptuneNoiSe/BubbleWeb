a53456ca7a3f33ebbc71d5519f57899b02edd3ad
d.a.arhipov <megadimension@Yandex.ru>
2023-10-03 16:25:11 +0300

    Function "random massive" working from now

--- app/controllers/random_controller.rb
+++ app/controllers/random_controller.rb
@@ -1,6 +1,7 @@
 class RandomController < ApplicationController
   layout 'random'
   def new
+    @result_random = Massive.send(:params, *params[:nod], *params[:min], *params[:max], *params[:ndp])
     render :index
   end
 end
